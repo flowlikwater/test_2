a411f7530ad0e3b29b6ae0ef124015648c77f18c
yyp <1564273611@qq.com>
2022-05-15 23:13:15 +0800

    新添了login

--- 1.js
+++ 1.js
@@ -1,5 +1,7 @@
-let h1 = document.querySelector('h1')
-h1.addEventListener('click', function () {
-    this.style.backgroundColor = 'red'
+// let h1 = document.querySelector('h1')
+// h1.addEventListener('click', function () {
+//     this.style.backgroundColor = 'red'
+// });
 
-})
+// let username = document.querySelector('.eee');
+// console.log(username);
